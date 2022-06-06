94506e0bb8762a2ef0ae55475235374f65f91291
DangVuDinhKhang <khangb1913234@student.ctu.edu.vn>
2022-06-06 15:19:44 +0700

    Fix port

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express")
 const cors = require("cors")
 const app = express()
-const setupStudentRoutes = require("./app/routes/student.route");
-const setupNotificationRoutes = require("./app/routes/notification.route")
+// const setupStudentRoutes = require("./app/routes/student.route");
+// const setupNotificationRoutes = require("./app/routes/notification.route")
 
 app.use(cors())
 app.use(express.json())
