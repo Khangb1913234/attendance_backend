6c9006871b57062bf63b088f9830c184c936699b
DangVuDinhKhang <khangb1913234@student.ctu.edu.vn>
2022-06-09 10:53:01 +0700

    Add JWT

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = config.app.port
 
 mongoose.connect(config.db.uri)
     .then(function(){
-        console.log("Connect to the database")
+        console.log("Connect to the local database")
     })
     .catch(function(err){
         console.log("Can not connect to the database", err)
